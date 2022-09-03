785bc2c8e75b99ffe4f278cfbceb1ef5cd703db4
Harrsh Patel <me.harrsh.patel@gmail.com>
2022-09-03 16:01:47 +0530

    Added Base Web

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { BaseProvider, DarkTheme } from 'baseui';
+import { Client as Styletron } from 'styletron-engine-atomic';
+import { Provider as StyletronProvider } from 'styletron-react';
+
+const engine = new Styletron();
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <StyletronProvider value={engine}>
+      <BaseProvider theme={DarkTheme}>
+        <p>Hello World!</p>
+      </BaseProvider>
+    </StyletronProvider>
+  );
 };
 
 export default App;
